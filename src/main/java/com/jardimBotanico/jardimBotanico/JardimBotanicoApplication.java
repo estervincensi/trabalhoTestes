4bfea56d483af831e0e950096c068964d933e20a
augusto.moovin <augusto.marlon@moovin.com.br>
2017-05-31 18:39:01 -0300

    first commit

---
+++ src/main/java/com/jardimBotanico/jardimBotanico/JardimBotanicoApplication.java
@@ -0,0 +1,12 @@
+package com.jardimBotanico.jardimBotanico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JardimBotanicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JardimBotanicoApplication.class, args);
+	}
+}

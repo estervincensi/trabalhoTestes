44fa4f1b41ff75a7076bd2d6558a5fa7f73b0d32
augustoberwaldt <augusto.berwaldt@gmail.com>
2017-06-11 15:42:36 -0300

    add cadastro de usuarios

--- src/main/java/com/jardimBotanico/jardimBotanico/controller/AccountController.java
+++ src/main/java/com/jardimBotanico/jardimBotanico/controller/AccountController.java
@@ -1,9 +1,14 @@
 package com.jardimBotanico.jardimBotanico.controller;
 
 import com.jardimBotanico.jardimBotanico.domain.Account;
+import com.jardimBotanico.jardimBotanico.service.AccountService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 /**
  * Created by harley on 10/06/2017.
@@ -12,6 +17,13 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class AccountController {
 
+    private final AccountService accountService;
+
+
+      public AccountController(AccountService accountService) {
+         this.accountService =  accountService;
+      }
+
       @GetMapping("/")
       public ModelAndView create() {
           return new ModelAndView("register")
@@ -19,7 +31,8 @@ public class AccountController {
       }
 
     @GetMapping("/save")
-    public void save() {
-      
+    public String save(@Valid Account operador, BindingResult bindingResult, Model model) {
+        this.accountService.save(operador);
+        return "redirect:/";
     }
 }
--- src/main/java/com/jardimBotanico/jardimBotanico/service/AccountService.java
+++ src/main/java/com/jardimBotanico/jardimBotanico/service/AccountService.java
@@ -2,10 +2,12 @@ package com.jardimBotanico.jardimBotanico.service;
 
 import com.jardimBotanico.jardimBotanico.domain.Account;
 import com.jardimBotanico.jardimBotanico.repository.AccountRepository;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by harley on 11/06/2017.
  */
+@Service
 public class AccountService {
 
     private final AccountRepository accountRepository;

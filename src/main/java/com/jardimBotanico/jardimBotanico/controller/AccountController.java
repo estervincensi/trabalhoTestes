946fa3ff94bf7c86cd2ca1ccb816cb83365e95af
augustoberwaldt <augusto.berwaldt@gmail.com>
2017-06-11 12:13:09 -0300

    add cadastro de usuarios

--- src/main/java/com/jardimBotanico/jardimBotanico/controller/AccountController.java
+++ src/main/java/com/jardimBotanico/jardimBotanico/controller/AccountController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AccountController {
 
-      @GetMapping("/test")
+      @GetMapping("/")
       public String create() {
           return "register";
       }

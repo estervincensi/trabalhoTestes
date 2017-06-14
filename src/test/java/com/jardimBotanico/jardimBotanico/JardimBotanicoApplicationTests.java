8bb7cda5ec229a3be1697ab449aa751c54d21efc
augustoberwaldt <augusto.berwaldt@gmail.com>
2017-06-14 08:36:38 -0300

    testes

--- src/test/java/com/jardimBotanico/jardimBotanico/JardimBotanicoApplicationTests.java
+++ src/test/java/com/jardimBotanico/jardimBotanico/JardimBotanicoApplicationTests.java
@@ -1,7 +1,10 @@
 package com.jardimBotanico.jardimBotanico;
 
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,34 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class JardimBotanicoApplicationTests {
 
+
+    private WebDriver webDriver;
+
+
+	@Before
+	public void setup() {
+
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\External\\chromedriver.exe");
+		webDriver = new ChromeDriver();
+	}
+
+	@AfterClass
+	public static void createAndStopService() {
+		//service.stop();
+	}
+
+	@After
+	public void quitDriver() {
+	//	webDriver.quit();
+	}
+
+
 	@Test
 	public void contextLoads() {
+
+
+		webDriver.get("http://www.google.com/xhtml");
+
 	}
 
 }
